c4bfa28f6b7086e9a5de14ae005bc8be1540115f
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-09 17:00:46 -0500

    Add API functionality for class, monster; array functionality for value

--- assets/tales.js
+++ assets/tales.js
@@ -3,10 +3,10 @@
 // function to establish character race
 
 let raceSelect = Math.ceil(Math.random() * 9)
-let uri = "https://www.dnd5eapi.co/api/races/"
+let apiRace = "https://www.dnd5eapi.co/api/races/"
 let race = document.querySelector("#race")
 
-fetch(uri)
+fetch(apiRace)
     .then(res => res.json())
     .then(data => {
         let randomIndex = Math.floor(Math.random()*data.results.length)
@@ -16,6 +16,39 @@ fetch(uri)
 
 // function to establish character class
 
+let classSelect = Math.ceil(Math.random() * 12)
+let apiClass = "https://www.dnd5eapi.co/api/classes/"
+let heroClass = document.querySelector("#class")
+
+fetch(apiClass)
+    .then(res => res.json())
+    .then(data => {
+        let randomIndex = Math.floor(Math.random()*data.results.length)
+        heroClass.textContent = data.results[randomIndex].index;
+        console.log(data)
+    })
+
 // function to establish monster
 
+let monsterSelect = Math.ceil(Math.random() * 332)
+let apiMonster = "https://www.dnd5eapi.co/api/monsters/"
+let monster = document.querySelector("#monster")
+
+fetch(apiMonster)
+    .then(res => res.json())
+    .then(data => {
+        let randomIndex = Math.floor(Math.random()*data.results.length)
+        monster.textContent = data.results[randomIndex].name.toLowerCase();
+        console.log(data)
+    })
+
 // function to establish value
+
+let valueSet = ["honor", "love", "untold riches", "fame", "the truth", "faith", "adventure", "beauty", "balance", "justice", "virtue", "friendship", "liberty", "the pursuit of happiness", "fun", "influence", "knowledge", "general mischief", "loyalty", "pleasure", "recognition", "peace", "security", "self-respect", "stability", "family", "status", "wisdom", "vengeance"]
+let valueSelect = Math.ceil(Math.random() * valueSet.length)
+let value = document.querySelector("#value")
+
+let randomIndex = Math.floor(Math.random() * valueSet.length)
+console.log(randomIndex)
+value.textContent = valueSet[randomIndex];
+console.log(valueSet[randomIndex])
